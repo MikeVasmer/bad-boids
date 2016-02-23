d40c2f79ba3c5b447b404cfee7c3d5898a757eee
Michael Vasmer <michael.vasmer.15@ucl.ac.uk>
2016-02-23 15:10:55 +0000

    Replaced magic numbers with constants

--- boids/boids.py
+++ boids/boids.py
@@ -9,33 +9,43 @@ import random
 
 # Deliberately terrible code for teaching purposes
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+min_x_position, max_x_position = -450, 50
+min_y_position, max_y_position = 300, 600
+min_x_velocity, max_x_velocity = 0, 10
+min_y_velocity, max_y_velocity = -20, 20
+numberOfBirds = 50
+
+boids_x=[random.uniform(min_x_position,max_x_position) for x in range(numberOfBirds)]
+boids_y=[random.uniform(min_y_position,max_y_position) for x in range(numberOfBirds)]
+boid_x_velocities=[random.uniform(min_x_velocity,max_x_velocity) for x in range(numberOfBirds)]
+boid_y_velocities=[random.uniform(min_y_velocity,max_y_velocity) for x in range(numberOfBirds)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
 	xs,ys,xvs,yvs=boids
 	# Fly towards the middle
+	move_middle_strength = 0.01
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
+			xvs[i]=xvs[i]+(xs[j]-xs[i])*move_middle_strength/len(xs)
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+			yvs[i]=yvs[i]+(ys[j]-ys[i])*move_middle_strength/len(xs)
 	# Fly away from nearby boids
+	min_separation_squared = 100
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < min_separation_squared:
 				xvs[i]=xvs[i]+(xs[i]-xs[j])
 				yvs[i]=yvs[i]+(ys[i]-ys[j])
 	# Try to match speed with nearby boids
+	nearby_distance_squared = 10000
+	matching_strength = 0.125
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < nearby_distance_squared:
+				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*matching_strength/len(xs)
+				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*matching_strength/len(xs)
 	# Move according to velocities
 	for i in range(len(xs)):
 		xs[i]=xs[i]+xvs[i]
