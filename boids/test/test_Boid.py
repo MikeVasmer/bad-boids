8b2ae8f537ecee505244c0da109443649c592ea3
Michael Vasmer <michael.vasmer.15@ucl.ac.uk>
2016-02-26 12:04:08 +0000

    Improve test for Boid class

--- boids/test/test_Boid.py
+++ boids/test/test_Boid.py
@@ -1,23 +1,19 @@
-from nose.tools import assert_equal
 from boids.boids import Boid
-import random
 import yaml
 import os
-from mock import call, patch
+from nose.tools import assert_true
 
-@patch("random.uniform")
-def test_init(mock_random_uniform):
+def test_init():
     test_params = yaml.load(
         open(
             os.path.join(
                 os.path.dirname(__file__),
                 'fixtures/test_params.yaml')))
     boid_limits = test_params["boid_params"]
-    test_boid = Boid(boid_limits)
-    calls = [
-        call(
-            boid_limits["min_x_position"], boid_limits["max_x_position"]), call(
-            boid_limits["min_y_position"], boid_limits["max_y_position"]), call(
-                boid_limits["min_x_velocity"], boid_limits["max_x_velocity"]), call(
-                    boid_limits["min_y_velocity"], boid_limits["max_y_velocity"])]
-    mock_random_uniform.assert_has_calls(calls)
+    # Run the test a few times to make sure an error is detected
+    for i in range(test_params["test_iterations"]):
+        test_boid = Boid(boid_limits)
+        assert_true(boid_limits["min_x_position"] <= test_boid.position[0] <= boid_limits["max_x_position"])
+        assert_true(boid_limits["min_y_position"] <= test_boid.position[1] <= boid_limits["max_y_position"])
+        assert_true(boid_limits["min_x_velocity"] <= test_boid.velocity[0] <= boid_limits["max_x_velocity"])
+        assert_true(boid_limits["min_y_velocity"] <= test_boid.velocity[1] <= boid_limits["max_y_velocity"])
